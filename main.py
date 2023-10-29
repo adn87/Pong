c1f922dc4ed84e6d7fa34426c76699a798ecbe6d
adn87 <aaghadieh@gmail.com>
2023-10-29 06:09:51 -0400

    Pong_003
    - created another that responds to key Presses

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from turtle import Screen
 from paddle import Paddle
 
+
+R_PADDLE_COR = (350,0)
+L_PADDLE_COR = (-350,0)
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 screen.listen()
-paddle = Paddle()
+
+r_paddle = Paddle((R_PADDLE_COR))
+l_paddle = Paddle((L_PADDLE_COR))
 
 
-screen.onkey(key="Up", fun=paddle.up)
-screen.onkey(key="Down", fun=paddle.down)
+screen.onkey(key="Up", fun=r_paddle.up)
+screen.onkey(key="Down", fun=r_paddle.down)
+
+screen.onkey(key="w", fun=l_paddle.up)
+screen.onkey(key="s", fun=l_paddle.down)
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -1,25 +1,22 @@
 from turtle import Turtle
 
-X_POS = 350
-Y_POS = 0
 
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.create_paddle(position)
 
-class Paddle:
-    def __init__(self):
-        self.paddle = Turtle()
-        self.create_paddle()
-
-    def create_paddle(self):
-        self.paddle.shape("square")
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.penup()
-        self.paddle.goto(x=X_POS, y=Y_POS)
+    def create_paddle(self,position):
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def up(self):
-        y_cor = self.paddle.ycor() + 20
-        self.paddle.goto(y=y_cor,x=self.paddle.xcor())
+        y_cor = self.ycor() + 20
+        self.goto(y=y_cor, x=self.xcor())
 
     def down(self):
-        y_cor = self.paddle.ycor() - 20
-        self.paddle.goto(y=y_cor, x=self.paddle.xcor())
+        y_cor = self.ycor() - 20
+        self.goto(y=y_cor, x=self.xcor())
